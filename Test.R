b88cd7e3fa02dfe369c225f1a3a9ee799af3df9f
Sal-Fakoya <salamot.fakoya08@gmail.com>
2024-10-15 16:10:02 -0500

    reading_and_observing_dataset

--- Test.R
+++ Test.R
@@ -1,4 +1,50 @@
-print("Test")
+# import necessary libraries:
+library(readr)
+library(dplyr)
 
-a <- 11
-print(a)
+# observe Dataset netflix_shows.test.csv
+netflix_shows <- read_csv("netflix_shows_set.csv")
+the_numbers <- read_csv("theNumbers_set.csv")
+top_10k <- read_csv("top_10k_set.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
