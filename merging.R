9f1d30e52f6462d2669a759c42d1b20686208ba0
Sal-Fakoya <salamot.fakoya08@gmail.com>
2024-10-20 02:14:11 -0500

    suggestion_on_next_steps

--- merging.R
+++ merging.R
@@ -1,5 +1,51 @@
 
-# To merge 
+# To merge datasets:
+
+# ------------------------------------------------------------------
+# Dataset has been transforemd using log-transformation. Log-transformation 
+# compress data, especially extreme values. It scales them down to make the data more manageable and less skewed.
+
+# Next Steps:
+
+# 1: Perform Descriptive Statistics: Get a description and insight from our data
+
+# 2: Visualize Descriptive Statistics : Visualize the described data
+
+# 3: Get a Good Sample and Normalize the data: Get the normal distribution 
+
+# 4: Perform Inferential Statistics: Deal with Uncertainty and Test the Sample.
+        # Suggestion: Look into Hypothesis Testing, One-Sample t-test, Two Sample  
+        #              tests,  Tests for Proportions
+
+# 5: Find the Correlation and Regression between Multiple Variables:
+        #  Suggestion: Look into Scatterplots, Heatmap, Corrplots 
+
+
+# ---------------------------------------------------------------------
+# 1. Perform Descriptive Statistics: 
+# (a) The Center of the data : Mean, Median, Mode (We don't need Mode just in case)?
+  # example: What is the [insert tool e.g "mean"] of the [insert numerical      
+              # variable e.g "revenue"]:
+  # General Question: for all movies in all years? for all movies in each year?
+  # Categorized question: for all movies in each genre? for each genre in each year?
+
+# (b) Find the Measures of Dispersion:
+        # Suggestion: Get the Range, Interquartile Range, Standard Deviation,
+        # Variance, Coefficient of Variation? What do they tell us?
+
+# (c) Find the Measures of Spread/Dispersion: How?
+    # Suggestion: Look into Box-Plots, Violin Plots, Histogram
+
+
+# (d) What is the Shape of the data? Define the Skewness of the data. How?
+    # Suggestion: Every distribution can be standardized. Standardize the (log-transformed) datasets then visualize the standardized dataset to see the distribution.
+
+
+
+
+
+
+
 
 
 
